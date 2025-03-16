b3b0ffabb22cc1a73201eccb52495b77f7ec6f4f
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-03-16 19:28:10 +0600

    sort vector

---
+++ vector/a.cpp
@@ -0,0 +1,13 @@
+
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+        vector <int> v = {1,3,4,3,56,4,3,65,5};
+        sort(v.begin(),v.end());
+        for(auto a:v)
+        cout<< a <<" "<<endl;
+        
+        return 0;
+      
+    }
