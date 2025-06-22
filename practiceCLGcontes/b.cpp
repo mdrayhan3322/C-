83ec6a903dc8ab27672dceb1585692b8bb54e50a
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 16:26:01 +0600

    pass fail

---
+++ practiceCLGcontes/b.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  
+    int mark;
+    cin>>mark;
+    if(mark >= 60) cout<<"pass korlo exam a";
+    else cout<<"pass korlo na exam a";
+    return 0;
+}
