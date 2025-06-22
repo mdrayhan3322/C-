017990c7ea0134d0b8e2b0298239c7ff03e72fb4
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 17:20:06 +0600

    even odd nubmer

---
+++ practiceCLGcontes/d.cpp
@@ -0,0 +1,10 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int number;
+    cin>>number;
+    if(number % 2 == 0 )cout<<"even number"<<endl;
+    else cout<<"odd number";
+    return 0;
+}
