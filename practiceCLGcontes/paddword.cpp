2272d83f0cdecdc5fd14099dc06f5fed3e634433
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 17:32:13 +0600

    pass word chack

---
+++ practiceCLGcontes/paddword.cpp
@@ -0,0 +1,9 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string password = "12234";
+    if(password == "12234")cout<<"login successful";
+    else cout<<"worng password"<<endl;
+    return 0;
+}
