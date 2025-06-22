0a77b0a21d96b9e56c7d0e525aad73b0c69b01cc
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 16:13:02 +0600

    chack numver

---
+++ practiceCLGcontes/a.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    if(num > 0){
+        cout<<"num akta donaktok sonka"<<endl;
+    }
+    else if (num < 0){
+        cout<<"num a renattok sonka"<<endl;
+    }
+    else cout<<"num akti 0";
+
+
+    return 0;
+}
--- vector/a.cpp
+++ vector/a.cpp
@@ -1,13 +1,28 @@
 
 
+    // #include<bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+    //     vector <int> v = {1,3,4,3,56,4,3,65,5};
+    //     sort(v.begin(),v.end());
+    //     for(auto a:v)
+    //     cout<< a <<" "<<endl;
+        
+    //     return 0;
+      
+    // }
+    // ----------------------------------------
     #include<bits/stdc++.h>
     using namespace std;
     int main(){
-        vector <int> v = {1,3,4,3,56,4,3,65,5};
-        sort(v.begin(),v.end());
-        for(auto a:v)
-        cout<< a <<" "<<endl;
+        vector<int>v= {1,3,2,4,5,3,6,4,4};
         
+        sort(v.begin()-1,v.end()-2);
+         
+        for(auto  singel_valu : v){
+           
+            cout<<singel_valu<<"    ";
+
+        }
         return 0;
-      
     }
