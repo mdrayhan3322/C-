d069d29f399a54a17ce97e19f18e4ad56d68e157
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 16:41:13 +0600

    vote

---
+++ practiceCLGcontes/c.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    if(age >= 18)cout<<"you can vote";
+    else cout<<"too young to vote";
+
+    return 0;
+}
