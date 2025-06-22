843866d12d0dab9bd837f412b0048a8b20f7399c
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 17:23:39 +0600

    big number

---
+++ practiceCLGcontes/e.cpp
@@ -0,0 +1,11 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>b)cout<<"a is big nubmer";
+    else cout<<"b is big number";
+    return 0;
+}
