8d33279c0e7bde2765d0f712e76eb7206ba33bb1
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-06-22 19:51:25 +0600

    number serse

---
+++ practiceCLGcontes/f.cpp
@@ -0,0 +1,10 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    if(num >=1 && num <= 10)cout<<"number ti 10 ar modde ase";
+    else cout<<"number ti 10 ar modde nai";
+    return 0;
+}
